c77a561cd432e3ac47275235d74bbb23f8cff36e
Hthiunhi <62916555+Hthiunhi@users.noreply.github.com>
2020-04-09 19:23:43 +0700

    1.0

---
+++ app/src/main/java/nv/edu/ntu/uyennhi/uyennhi_59131707_helloworld/MainActivity.java
@@ -0,0 +1,15 @@
+package nv.edu.ntu.uyennhi.uyennhi_59131707_helloworld;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
